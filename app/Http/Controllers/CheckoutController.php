d52e150d9cc4c7bdf0582b3fe9a53e1139941093
Ryan <ryannarace@gmail.com>
2026-04-08 15:29:08 -0400

    Implemented checkout page and started checkout functuinality

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Traits\PhpFlasher;
+use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
 {
     use PhpFlasher;
+
+    public function index()
+    {
+
+        $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        $user = Auth::user();
+
+        $cart_data = $user->products()->withPrices()->get();
+
+        if ($cart_data->isEmpty()) {
+            return redirect()->route('cart.index')->with('message', 'Your cart is empty');
+        }
+
+        $cart_data->calculateSubtotal();
+
+        return view('pages.default.checkoutpage', compact('cart_data'));
+    }
 }
--- app/Http/Controllers/CheckoutPaymentController.php
+++ app/Http/Controllers/CheckoutPaymentController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ShippingHelper;
+use App\Helpers\StripeCheckout;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+
 
 class CheckoutPaymentController extends Controller
 {
@@ -15,6 +20,59 @@ class CheckoutPaymentController extends Controller
      */
     public function index($payment)
     {
+
+        // Get groups
+        $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        // Get user
+        $user = Auth::user();
+
+        //Create variables
+        $shipping_helper = new ShippingHelper();
+        $stripe_checkout = new StripeCheckout();
+        $order = new Order();
+        $insert_data = [];
+        $compeleted = false;
+
+        // Get products
+        $cart_data = $user->products()->withPrices()->get();
+
+
+        // Check if cart is empty
+        if ($cart_data->isEmpty()) {
+            return redirect()->route('cart.index')->with('message', 'Your cart is empty');
+        }
+
+        // Get subtotal
+        $cart_data->calculateSubtotal();
+
+        // Determine payment
+        switch ($payment) {
+            case 'stripe':
+                # code...
+                break;
+
+            default:
+                $inster_date = [
+                    'payment_provider' => 'testing',
+                    'payment_id' => 'testing',
+                ];
+                $completed = true;
+                break;
+        }
+
+        // Validate
+        if (!$completed || empty($inster_data)) {
+            dd('Payment is incomplete or checkout is missing');
+        }
+
+        // Create order details
+
+        // Create order details
+
+        // Redirect
+
+
         return true;
     }
 }
