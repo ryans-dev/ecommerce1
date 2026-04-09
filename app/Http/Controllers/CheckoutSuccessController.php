35d0a7092bfaa33991cbe3e276b27f27f4bc2218
Ryan <ryannarace@gmail.com>
2026-04-09 01:33:03 -0400

    Completed application main functionalities
    Store, details, cart, checkout and checkout success

--- app/Http/Controllers/CheckoutSuccessController.php
+++ app/Http/Controllers/CheckoutSuccessController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\StripeCheckoutSuccess;
 use App\Traits\PhpFlasher;
 
 class CheckoutSuccessController extends Controller
 {
     use PhpFlasher;
+
+    public function index($id)
+    {
+        $stripe_check = new StripeCheckoutSuccess();
+        $successful = $stripe_check->updateCheckoutOrder($id);
+        if (!$successful) {
+            abort(404);
+        }
+
+        return view('pages.default.checkout-successpage');
+    }
 }
